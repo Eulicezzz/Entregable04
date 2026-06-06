77a4740750c3290aa4410460070ad23abbaa9790
Eulicezzz <xaviergerdez@gmail.com>
2026-06-06 01:41:29 -0500

    avance de la estructura inicial del login

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,27 @@
+import {createContext, useState, useEffect} from 'react';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({children}) => {
+    // Intentamos recuperar el token del localStorage al iniciar la aplicación
+    const [token, setToken] = useState(localStorage.getItem('token') || null);
+    const [isAuthenticated, setIsAuthenticated] = useState(!!token);
+
+    const login = (newToken) => {
+        localStorage.setItem('token', newToken);
+        setToken(newToken);
+        setIsAuthenticated(true);
+    };
+
+    const Logout = () => {
+        localStorage.removeItem('token');
+        setToken(null);
+        setIsAuthenticated(false);
+    };
+
+    return (
+        <AuthContext.Provider value={{ token, isAuthenticated, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
