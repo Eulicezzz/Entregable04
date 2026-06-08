c8a15a1f81fa03638ad7c2126e7f8b635ae82689
Eulicezzz <xaviergerdez@gmail.com>
2026-06-08 10:09:44 -0500

    cración de variables de entorno

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,9 +1,19 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+#Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
+USER = os.getenv("DB_USER")
+PASSWORD = os.getenv("DB_PASSWORD")
+HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
 
 #Estructura: mysql+mysqlconnector://user:password@host/db_name
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:root@localhost/municipalidad"
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{USER}:{PASSWORD}@{HOST}/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
